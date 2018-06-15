daa276a3f0da449f5ace0b24d9add8e412eb7bab
Dontavious Sanders <sanddc178@gmail.com>
2018-06-15 12:12:32 -0500

    Installed postcss. Tested some plugins to make sure they're being used

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,29 @@
 var gulp = require('gulp')
 var watch = require('gulp-watch')
+var postcss = require('gulp-postcss')
+var autoprefixer = require('autoprefixer')
+var cssvars = require('postcss-simple-vars')
+var nested = require('postcss-nested')
 
 gulp.task('default', function() {
   console.log('Hooray, you created a Gulp task')
 })
 
-gulp.task('html', function() {
-  console.log('Imagine something useful being done to your HTML here')
-})
+// gulp.task('html', function() {
+//   console.log('Imagine something useful being done to your HTML here')
+// })
 
 gulp.task('styles', function() {
-  console.log('Imagine SASS or PostCSS tasks running here')
+  return gulp
+    .src('./app/assets/styles/styles.css')
+    .pipe(postcss([nested, cssvars, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles'))
 })
 
 gulp.task('watch', function() {
-  watch('./app/index.html', function() {
-    gulp.start('html')
-  })
+  // watch('./app/index.html', function() {
+  //   gulp.start('html')
+  // })
 
   watch('./app/assets/styles/**/*.css', function() {
     gulp.start('styles')

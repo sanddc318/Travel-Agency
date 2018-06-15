8e7bfa74f24796ac623dab081abc7336139890e5
Dontavious Sanders <sanddc178@gmail.com>
2018-06-15 14:16:33 -0500

    Configured browserSync to automate all the things

--- gulpfile.js
+++ gulpfile.js
@@ -2,31 +2,39 @@ var gulp = require('gulp')
 var watch = require('gulp-watch')
 var postcss = require('gulp-postcss')
 var autoprefixer = require('autoprefixer')
-var cssvars = require('postcss-simple-vars')
+var cssVars = require('postcss-simple-vars')
 var nested = require('postcss-nested')
-var cssimport = require('postcss-import')
+var cssImport = require('postcss-import')
+var browserSync = require('browser-sync').create()
 
 gulp.task('default', function() {
   console.log('Hooray, you created a Gulp task')
 })
 
-// gulp.task('html', function() {
-//   console.log('Imagine something useful being done to your HTML here')
-// })
-
 gulp.task('styles', function() {
   return gulp
     .src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssimport, cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, cssVars, nested, autoprefixer]))
     .pipe(gulp.dest('./app/temp/styles'))
 })
 
+gulp.task('cssInject', ['styles'], function() {
+  return gulp.src('./app/temp/styles/styles.css').pipe(browserSync.stream())
+})
+
 gulp.task('watch', function() {
-  // watch('./app/index.html', function() {
-  //   gulp.start('html')
-  // })
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: 'app'
+    }
+  })
+
+  watch('./app/index.html', function() {
+    browserSync.reload()
+  })
 
   watch('./app/assets/styles/**/*.css', function() {
-    gulp.start('styles')
+    gulp.start('cssInject')
   })
 })

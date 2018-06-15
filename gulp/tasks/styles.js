c21d41b01be972346d3d0b82bf86cda25b0ef50f
Dontavious Sanders <sanddc178@gmail.com>
2018-06-15 15:00:27 -0500

    Went ahead and added mixins for different screens

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,11 +4,12 @@ var autoprefixer = require('autoprefixer')
 var cssVars = require('postcss-simple-vars')
 var nested = require('postcss-nested')
 var cssImport = require('postcss-import')
+var mixins = require('postcss-mixins')
 
 gulp.task('styles', function() {
   return gulp
     .src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport, cssVars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, mixins, cssVars, nested, autoprefixer]))
     .on('error', function(errorInfo) {
       console.log(errorInfo.toString())
       this.emit('end')

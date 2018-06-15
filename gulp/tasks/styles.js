40f39bed28bb06146e36c8ca02a1bbdcd72cbc65
Dontavious Sanders <sanddc178@gmail.com>
2018-06-15 14:31:43 -0500

    Added persistance to gulp

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -9,5 +9,9 @@ gulp.task('styles', function() {
   return gulp
     .src('./app/assets/styles/styles.css')
     .pipe(postcss([cssImport, cssVars, nested, autoprefixer]))
+    .on('error', function(errorInfo) {
+      console.log(errorInfo.toString())
+      this.emit('end')
+    })
     .pipe(gulp.dest('./app/temp/styles'))
 })
